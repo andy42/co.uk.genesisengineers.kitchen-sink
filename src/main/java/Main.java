dfd59fa6dbee9de167f882d475f39e3d75471e7c
andy42 <andy@jaehl.com>
2019-04-07 21:15:20 +0100

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    private void run (String[] args) {
+        System.out.print("test");
+    }
+
+    public static void main (String[] args) {
+        new Main().run(args);
+    }
 }
