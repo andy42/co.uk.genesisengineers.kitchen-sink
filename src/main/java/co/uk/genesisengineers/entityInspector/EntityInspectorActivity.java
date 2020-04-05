15484fe32d57e508fe2e9188db33a4dc9f9501b8
Andy42 <andy@jaehl.com>
2020-04-05 17:07:24 +0100

    added layout xml schema

--- src/main/java/co/uk/genesisengineers/entityInspector/EntityInspectorActivity.java
+++ src/main/java/co/uk/genesisengineers/entityInspector/EntityInspectorActivity.java
@@ -1,6 +1,18 @@
 package co.uk.genesisengineers.entityInspector;
 
+import co.uk.genesisengineers.kitchenSink.R;
 import ui.activity.Activity;
+import ui.view.LayoutInflater;
+import ui.view.TextView;
 
 public class EntityInspectorActivity  extends Activity {
+
+    private TextView nameTextView;
+    @Override
+    public void onCreate () {
+        LayoutInflater layoutInflater = new LayoutInflater();
+        this.view = layoutInflater.inflate(this, R.layouts.activity_entity_inspector_xml, null);
+
+        nameTextView = (TextView)this.view.findViewById("name");
+    }
 }
